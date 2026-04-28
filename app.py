1faf83fc7f316fab87af8ab88184f6ca418d316c
FacuTaTin123 <d47663617@alumnos.itr.edu.ar>
2026-04-28 08:09:55 -0300

    ahi ta

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("Contacto.html")
+
+@app.route("/base")
+def nashe():
+    return render_template("base.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
